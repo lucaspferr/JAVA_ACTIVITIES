1aa5a480ea0f30e6265bf4801a058e6f69587646
Lucas Pimentel Ferreira <lucas.pferr194@gmail.com>
2022-01-28 19:02:52 -0300

    Projeto Blackjack (21)

---
+++ Calculadora_Interf/src/calculadora_interf/Calculadora_intController.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package calculadora_interf;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author lucas
+ */
+public class Calculadora_intController implements Initializable {
+
+    @FXML
+    private Button botao1;
+    @FXML
+    private Button botao2;
+    @FXML
+    private Button botao3;
+    @FXML
+    private Button botao4;
+    @FXML
+    private Button botao5;
+    @FXML
+    private Button botao6;
+    @FXML
+    private Button botao7;
+    @FXML
+    private Button botao8;
+    @FXML
+    private Button botao9;
+    @FXML
+    private Button botao0;
+    @FXML
+    private Button botaoponto;
+    @FXML
+    private Button botaoigual;
+    @FXML
+    private Button botaomais;
+    @FXML
+    private Button botaomenos;
+    @FXML
+    private Button botaovezes;
+    @FXML
+    private Button botaodiv;
+    @FXML
+    private TextField output_calc;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handlebotao1(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotao2(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotao3(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotao4(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotao5(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotao6(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotao7(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotao8(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotao9(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotao0(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotaoponto(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotaoigual(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotaomais(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotaomenos(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotaovezes(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlebotaodiv(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleOutput_calc(ActionEvent event) {
+    }
+    
+}
