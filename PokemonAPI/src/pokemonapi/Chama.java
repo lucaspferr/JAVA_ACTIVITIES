2e9fe1f5fb2eb26dad6b6f2065e39fffa72278ed
Lucas Pimentel Ferreira <lucas.pferr194@gmail.com>
2022-01-31 17:07:38 -0300

    PokeAPI in Terminal - Lucas
    Using the PokeAPI to learn about API and it's uses.

---
+++ PokemonAPI/src/pokemonapi/Chama.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonapi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ * @author lucas
+ */
+public class Chama {
+    
+    private HttpURLConnection connection;
+    
+    
+    public void pokechama(){
+        
+        BufferedReader reader;
+        String  line;
+        StringBuffer responseContent = new StringBuffer();
+        try{
+            URL url = new URL("https://pokeapi.co/api/v2/berry/1");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("User-Agent", "foi");
+            //Setup de pedido
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            
+            int status = connection.getResponseCode();
+            System.out.println(status);
+            
+            if(status>299){
+                //Testa se nao foi possivel ler a pag ( pex 402,403,404)
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                while((line = reader.readLine())!=null){
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            else{
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while((line = reader.readLine())!=null){
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            System.out.println(responseContent.toString());
+        }
+        catch(MalformedURLException e){}
+        catch(IOException e){}
+        
+    }
+}
---
+++ PokemonAPI/src/pokemonapi/PokemonAPI.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pokemonapi;
+
+/**
+ *
+ * @author lucas
+ */
+public class PokemonAPI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Chama chama1 = new Chama();
+        chama1.pokechama();
+    }
+    
+}
