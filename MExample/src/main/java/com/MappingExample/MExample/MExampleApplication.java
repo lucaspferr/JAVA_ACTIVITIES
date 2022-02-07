c9e8237ffef6e9a36549e24689a1270bc83555a7
Lucas Pimentel Ferreira <lucas.pferr194@gmail.com>
2022-02-07 17:30:12 -0300

    Mapping Example
    Demo for testing mapping.

---
+++ MExample/src/main/java/com/MappingExample/MExample/MExampleApplication.java
@@ -0,0 +1,13 @@
+package com.MappingExample.MExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MExampleApplication.class, args);
+	}
+
+}
---
+++ MExample/src/test/java/com/MappingExample/MExample/MExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MappingExample.MExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
