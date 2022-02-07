e70ffbe9ff0139d02884463b975b003d2a429799
Lucas Pimentel Ferreira <lucas.pferr194@gmail.com>
2022-02-07 18:14:39 -0300

    Added External API

--- MExample/src/main/java/com/MappingExample/MExample/Controller/MExController.java
+++ MExample/src/main/java/com/MappingExample/MExample/Controller/MExController.java
@@ -2,6 +2,7 @@ package com.MappingExample.MExample.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MExController {
@@ -19,5 +20,12 @@ public class MExController {
             else b=b+"-"+a;
         }
         return b;
+    }
+    @GetMapping("/t3")
+    public String ExtAPI(){
+        String uri = "https://jsonplaceholder.typicode.com/todos/1";
+        RestTemplate exta = new RestTemplate();
+        String result = exta.getForObject(uri, String.class);
+        return result;
     }
 }
